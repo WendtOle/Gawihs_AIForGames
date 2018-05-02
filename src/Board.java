781fcbbaf9c03bf3d0099a6c9637b542104e1821
WendtOle <dielanie@freenet.de>
2018-05-02 15:54:30 +0200

    save own team number in board and transfer int[] to point

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,13 @@
 import lenz.htw.gawihs.Move;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Board {
+    int teamNumber;
     int [][] boardRepr = new int[9][9];
 
     // 1111 / 0xf   -> dont exist
@@ -17,39 +21,43 @@ public class Board {
     // 0101 ... player three and player one could go here
     // ...
 
-    int[][] neighbors = {
-            {0,1},{1,1},
-            {-1,0},{1,0},
-            {-1,-1},{0,-1}
+    Point[] neighbors = {
+            new Point(0,1),new Point(1,1),
+            new Point(-1,0),new Point(1,0),
+            new Point(-1,-1),new Point(0,-1)
     };
 
-    public Board(){
+    Point[][] teamPosition = {
+            {new Point(0,0),new Point(1,0),new Point(2,0),new Point(3,0),new Point(4,0)},
+            {new Point(0,4),new Point(1,5),new Point(2,6),new Point(3,7),new Point(4,8)},
+            {new Point(8,4),new Point(8,5),new Point(8,6),new Point(8,7),new Point(8,8)}
+    };
+
+    public Board(int teamNumber){
+        this.teamNumber = teamNumber;
         for (int[] row: boardRepr){
             Arrays.fill(row,0);
         }
 
-        int[][] nonExistingFields = {
-                {0,8},{1,8},{2,8},{3,8},
-                {0,7},{1,7},{2,7},
-                {0,6},{1,6},
-                {0,5},
-                {8,3},
-                {7,2},{8,2},
-                {6,1},{7,1},{8,1},
-                {5,0},{6,0},{7,0},{8,0}};
+        Point[] nonExistingFields = {
+                new Point(0,8), new Point(1,8), new Point(2,8), new Point(3,8),
+                new Point(0,7), new Point(1,7), new Point(2,7),
+                new Point(0,6), new Point(1,6),
+                new Point(0,5),
+                new Point(8,3),
+                new Point(7,2), new Point(8,2),
+                new Point(6,1), new Point(7,1), new Point(8,1),
+                new Point(5,0), new Point(6,0), new Point(7,0),new Point(8,0)};
         setField(nonExistingFields,0xff);
-        int[][] teamOnePlacement    = {{0,0},{1,0},{2,0},{3,0},{4,0}};
-        setField(teamOnePlacement,0x1);
-        int[][] teamTwoPlacement    = {{0,4},{1,5},{2,6},{3,7},{4,8}};
-        setField(teamTwoPlacement,0x2);
-        int[][] teamThreePlacement  = {{8,4},{8,5},{8,6},{8,7},{8,8}};
-        setField(teamThreePlacement,0x3);
 
+        for (int i = 0; i < teamPosition.length; i++) {
+            setField(teamPosition[i],i + 1);
+        }
     }
 
-    private void setField(int[][]coordinates, int valueToPlace){
-        for (int[] coordinate :coordinates){
-            boardRepr[coordinate[0]][coordinate[1]] = valueToPlace;
+    private void setField(Point[]coordinates, int valueToPlace){
+        for (Point coordinate :coordinates){
+            boardRepr[coordinate.x][coordinate.y] = valueToPlace;
         }
     }
 
--- test/BoardTest.java
+++ test/BoardTest.java
@@ -12,34 +12,34 @@ import static org.junit.Assert.assertArrayEquals;
 public class BoardTest {
 
     private Board board;
-    private int[] startField;
-    private int[][] expectedNeighbours;
+    private Point startField;
+    private Point[] expectedNeighbours;
     private String message;
 
     @Before
     public void before(){
-        board = new Board();
+        board = new Board(1);
     }
 
     @Parameterized.Parameters
     public static Collection data() {
         return Arrays.asList(new Object[][] {
-                {"normal_emptyBoard",new int[]{1,1},new int[][]{{1,2},{2,2},{0,1},{2,1},{0,0},{1,0}}},
-                {"onEdge_emptyBoard",new int[]{0,4},new int[][]{{1,5},{1,4},{0,3}}},
-                {"onEdge_emptyBoard",new int[]{2,6},new int[][]{{3,7},{3,6},{1,5},{2,5}}},
-                {"onEdge_emptyBoard",new int[]{6,2},new int[][]{{6,3},{7,3},{5,2},{5,1}}},
-                {"onEdge_emptyBoard",new int[]{0,0},new int[][]{{0,1},{1,1},{1,0}}},
-                {"onEdge_emptyBoard",new int[]{8,8},new int[][]{{7,8},{7,7},{8,7}}}
+                {"normal_emptyBoard",new Point(1,1),new Point[] {new Point(1,2),new Point(2,2),new Point(0,1),new Point(2,1),new Point(0,0),new Point(1,0)}},
+                {"onEdge_emptyBoard",new Point(0,4),new Point[]{new Point(1,5),new Point(1,4),new Point(0,3)}},
+                {"onEdge_emptyBoard",new Point(2,6),new Point[]{new Point(3,7),new Point(3,6),new Point(1,5),new Point(2,5)}},
+                {"onEdge_emptyBoard",new Point(6,2),new Point[]{new Point(6,3),new Point(7,3),new Point(5,2),new Point(5,1)}},
+                {"onEdge_emptyBoard",new Point(0,0),new Point[]{new Point(0,1),new Point(1,1),new Point(1,0)}},
+                {"onEdge_emptyBoard",new Point(8,8),new Point[]{new Point(7,8),new Point(7,7),new Point(8,7)}}
         });
     }
 
-    public BoardTest(String message, int[] input, int[][] expected) {
+    public BoardTest(String message, Point input, Point[] expected) {
         startField = input;
         expectedNeighbours = expected;
     }
 
     @Test
     public void test() {
-        assertArrayEquals(message, expectedNeighbours, board.getPossibleNeighborFields(startField));
+        assertArrayEquals(message, expectedNeighbours, board.getPossibleMoveToPositionFrom(startField).toArray());
     }
 }
